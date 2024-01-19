5eba47a71744b550f894ed322cbbbcd773c01010
ArielMichelli <arielm-04@hotmail.com>
2024-01-19 01:23:16 -0300

    Agregando mas propiedades al Context, agregado de RelatedProducts e CartItems

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+        In this Store you will find quality at a very affordable price, a wide variety of clothing. World-renowned brands and prestige, all just a click away. So don't think twice and buy that garment that you deserve so much, you know, choose this shopping portal and be guaranteed to wear something 100% original at the lowest price. DON'T think about it anymore!!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
